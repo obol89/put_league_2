0b738384c2f4165fd400a0e8a918f5eb16d92d54
aobolewicz <aobolewicz@egnyte.com>
2020-03-07 13:40:26 +0100

    add methods

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import itertools
 import random
+import csv
 
 
 def get_teams():
@@ -15,3 +16,15 @@ def get_team_pairs(teams):
     pairs = list(itertools.combinations(teams, 2))
     random.shuffle(pairs)
     return pairs
+
+def csv_export(pairs):
+    tournament = "/home/egnyte/tournament.csv"
+    with open(tournament, 'w', newline='') as tournament:
+        wr = csv.writer(tournament, quoting=csv.QUOTE_ALL)
+        wr.writerow(pairs)
+
+
+# Teams input
+# Generate csv with fixture
+# Add manually results in csv file
+# Import csv and generate table with points and choose winner
